a99cda1df0c85fa0b05d2fe24458026e60155729
Santy <santiago.gil@sinapsis.global>
2023-04-17 19:01:42 -0500

    ✨ first commit

---
+++ src/components/UserRepositories.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>{{ user }}</div>
+</template>
+
+<script>
+export default {
+    name: 'UserRepositories',
+    props: {
+        user: {
+            type: String,
+            required: true
+        },
+    },
+    setup(){
+        
+    },
+    data(){
+        return{
+            repositories: [], // 1
+            filters: {}, // 3
+            searchQuery: '', // 2
+        }
+    },
+    computed: {
+        filteredRepositories(){
+            // 3
+            return null
+        },
+        repositoriesMatchingSearchQuery(){
+            // 2
+            return null
+        },
+    },
+}
+
+</script>
