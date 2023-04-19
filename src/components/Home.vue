a349458e45540498bdd45f09c5a77f2dc0e21cda
Santy <santiago.gil@sinapsis.global>
2023-04-19 12:07:19 -0500

    ✨using computed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,37 @@
 <template>
-    <div>{{ obj }}</div>
+    <div>{{ fullName }}</div>
 </template>
 
 <script>
-import { reactive, watch } from 'vue';
+import { ref, computed } from 'vue';
 
     export default {
         setup(){
+            
+            const firstName = ref('Santy')
+            const lastName = ref('Gil')
 
-            const obj =  reactive({count: 0});
-
-            setInterval(() => obj.count++, 500)
-
-            watch(() => obj.count, (newValue,prevValue) => {
-                console.log(newValue,prevValue);
+            const fullName = computed(()=>{
+                return `${firstName.value} ${lastName.value}`
             })
 
-            return {
-                obj
-            };
+
+            // const obj =  reactive({count: 0});
+
+            // setInterval(() => obj.count++, 500)
+
+            // watch(() => obj.count, (newValue,prevValue) => {
+            //     console.log(newValue,prevValue);
+            // })
+
+            // return {
+            //     obj
+            // };
+
+            
+            return{
+                fullName
+            }
         },
     };
 </script>
